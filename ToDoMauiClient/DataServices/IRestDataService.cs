450fb2b7f6dd3e16f2ec70150e59b15d9a70db9a
Scott Vaughan <scott.vaughan@amido.com>
2022-08-24 14:52:09 +0100

    Add dataservice interface

---
+++ ToDoMauiClient/DataServices/IRestDataService.cs
@@ -0,0 +1,16 @@
+﻿using ToDoMauiClient.Models;
+
+namespace ToDoMauiClient.DataServices;
+
+public interface IRestDataService
+{
+    Task<List<ToDo>> GetAllToDosAsync();
+
+    Task<ToDo> GetToDoAsync(int id);
+
+    Task AddToDoAsync(ToDo toDo);
+
+    Task UpdateToDoAsync(ToDo toDo);
+
+    Task DeleteToDoAsync(int id);
+}
