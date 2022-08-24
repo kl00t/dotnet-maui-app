1a2dd64aa8d4042d68061b19eda1349c6088d984
Scott Vaughan <scott.vaughan@amido.com>
2022-08-24 14:25:53 +0100

    Add endpoints

--- ToDoAPI/Program.cs
+++ ToDoAPI/Program.cs
@@ -1,5 +1,6 @@
 using Microsoft.EntityFrameworkCore;
 using ToDoAPI.Data;
+using ToDoAPI.Models;
 
 var builder = WebApplication.CreateBuilder(args);
 
@@ -8,5 +9,55 @@ builder.Services.AddDbContext<AppDbContext>(opt => opt.UseSqlite(builder.Configu
 var app = builder.Build();
 
 //app.UseHttpsRedirection();
+
+app.MapGet("api/todo", async (AppDbContext context) =>
+{
+    var items = await context.ToDos.ToListAsync();
+    return Results.Ok(items);
+});
+
+app.MapGet("api/todo/{id}", async (AppDbContext context, int id) =>
+{
+    var toDoModel = await context.ToDos.FirstOrDefaultAsync(x => x.Id == id);
+    if (toDoModel == null)
+    {
+        return Results.NotFound();
+    }
+
+    return Results.Ok(toDoModel);
+});
+
+app.MapPost("api/todo", async (AppDbContext context, ToDo toDo) =>
+{
+    await context.ToDos.AddAsync(toDo);
+    await context.SaveChangesAsync();
+    return Results.Created($"api/todo/{toDo.Id}", toDo);
+});
+
+app.MapPut("api/todo/{id}", async (AppDbContext context, int id, ToDo toDo) =>
+{
+    var toDoModel = await context.ToDos.FirstOrDefaultAsync(x => x.Id == id);
+    if (toDoModel == null)
+    {
+        return Results.NotFound();
+    }
+
+    toDoModel.ToDoName = toDo.ToDoName;
+    await context.SaveChangesAsync();
+    return Results.Ok(toDoModel);
+});
+
+app.MapDelete("api/todo/{id}", async (AppDbContext context, int id) =>
+{
+    var toDoModel = await context.ToDos.FirstOrDefaultAsync(x => x.Id == id);
+    if (toDoModel == null)
+    {
+        return Results.NotFound();
+    }
+
+    context.ToDos.Remove(toDoModel);
+    await context.SaveChangesAsync();
+    return Results.NoContent();
+});
 
 app.Run();
