314a6394362a46403bd2e6f463da5573b4bf17e8
Scott Vaughan <scott.vaughan@amido.com>
2022-08-24 12:39:02 +0100

    Add connection string. Setup sqlite db

--- ToDoAPI/Program.cs
+++ ToDoAPI/Program.cs
@@ -1,4 +1,9 @@
+using Microsoft.EntityFrameworkCore;
+using ToDoAPI.Data;
+
 var builder = WebApplication.CreateBuilder(args);
+
+builder.Services.AddDbContext<AppDbContext>(opt => opt.UseSqlite(builder.Configuration.GetConnectionString("SqliteConnection")));
 
 var app = builder.Build();
 
